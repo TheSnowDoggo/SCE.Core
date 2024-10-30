f5b3df8de183b9cb94771d2a561bc463bff2c6a8
TheSnowDogggo <redstonewolfer@gmail.com>
2024-10-30 22:48:01 +0000

    merged SCECore with SCECoreInterfaces

---
+++ Interfaces/IProgram.cs
@@ -0,0 +1,18 @@
+﻿namespace SCECore.Interfaces
+{
+    /// <summary>
+    /// Interface for programs that contain multiple IAutoloadbles to be loaded into GameHandler.
+    /// </summary>
+    public interface IProgram
+    {
+        /// <summary>
+        /// Gets a list of all IAutoloadables in this instance.
+        /// </summary>
+        public List<IAutoloadable> AutoloadableList { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is active.
+        /// </summary>
+        public bool IsActive { get; }
+    }
+}
