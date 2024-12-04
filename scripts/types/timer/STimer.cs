5aa0b095df9456319624283a2f5d856fb03165ab
TheSnowDoggo <redstonewolfer@gmail.com>
2024-12-04 08:14:23 +0000

    timer c hange

--- scripts/types/timer/STimer.cs
+++ scripts/types/timer/STimer.cs
@@ -1,39 +1,65 @@
 ﻿namespace SCE
 {
     using System.Diagnostics;
-    using Seconds = Double;
 
     public class STimer
     {
         private readonly Stopwatch _stopwatch = new();
 
-        public STimer(Seconds length)
+        public STimer(double lengthSeconds)
         {
-            Length = length;
+            Length = lengthSeconds;
         }
 
-        public Seconds Length { get; set; }
+        public STimer(long lengthMilliseconds)
+        {
+            Length = (long)(lengthMilliseconds / 1000);
+        }
 
-        public Seconds Remaining { get => Length - _stopwatch.Elapsed.TotalSeconds; }
+        public double Length { get; set; }
+
+        public TimeSpan Elapsed { get => _stopwatch.Elapsed; }
+
+        public long ElapsedMilliseconds { get => _stopwatch.ElapsedMilliseconds; }
+
+        public long RemainingMilliseconds { get => (long)Length - _stopwatch.ElapsedMilliseconds; }
+
+        public double RemainingSeconds { get => Length - _stopwatch.Elapsed.TotalSeconds; }
+
+        public bool IsRunning { get => _stopwatch.IsRunning; }
 
         public event EventHandler? OnEndEvent;
 
-        public static STimer GetNew(Seconds length)
+        public static STimer GetNew(double length)
         {
             STimer timer = new(length);
             timer.Start();
             return timer;
         }
 
-        public void Start() { _stopwatch.Start(); }
+        public void Start()
+        {
+            _stopwatch.Start();
+        }
 
-        public void Stop() { _stopwatch.Stop(); }
+        public void Stop()
+        {
+            _stopwatch.Stop();
+        }
 
-        public void Restart() { _stopwatch.Restart(); }
+        public void Reset()
+        {
+            _stopwatch.Reset();
+        }
+
+        public void Restart()
+        {
+            _stopwatch.Restart();
+        }
 
         public void Update()
         {
-            if (Remaining <= 0)
+            if (RemainingSeconds <= 0)
                 OnEndEvent?.Invoke(this, EventArgs.Empty);
         }
     }
